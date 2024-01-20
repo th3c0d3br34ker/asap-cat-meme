983b4bea97a6cdcebbda64257b714da4aec0738c
th3c0d3br34ker <jainamdesai.learn@gmail.com>
2024-01-21 01:34:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,20 +4,11 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const requestLogger = require('./middlewares/requestLogger');
-
-mongoose.connect(process.env.MONGO_URI);
-
-mongoose.connection.once('open', () => {
-  console.log('📦 database connected');
-
-  app.listen(PORT, () => {
-    console.log('🚀 server started on port:', PORT);
-  });
-});
+const routes = require('./routers');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ?? 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -28,4 +19,14 @@ app.get('/', (req, res) => {
   res.send('o_O');
 });
 
-app.use('/api', require('./routers'));
+app.use('/api', routes);
+
+mongoose.connect(process.env.MONGO_URI);
+
+mongoose.connection.once('open', () => {
+  console.log('📦 database connected');
+
+  app.listen(PORT, () => {
+    console.log('🚀 server started on port:', PORT);
+  });
+});
